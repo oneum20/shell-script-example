9d43b43304b0f7cbd77b08aff55d8d5a5b040677
root <root@localhost.localdomain>
2021-01-20 00:21:20 +0900

    shell exam

--- 쉘스크립트 예제 - 도커 이미지 삭제/del-image.sh
+++ 쉘스크립트 예제 - 도커 이미지 삭제/del-image.sh
@@ -1,24 +1,51 @@
 #!/bin/bash
-
-# Variables
-IMAGE_PREFIX=${IMAGE_PREFIX:-}
-LIST_FILE_NAME=execlusion_list
-DELETE_TARGET=`docker images --format="{{.Repository}}:{{.Tag}}" | grep -v -E $(sed ':a;N;$!ba;s/\n/|/g' $LIST_FILE_NAME)`
-
+#===========================================
+#       Function
+#===========================================
 # Log format
 function log(){
   local timestamp=`date '+%Y-%m-%d %H:%M'`
   echo "[$timestamp]: $1"
 }
 
+function listToLine(){
+  echo "$1"
+  sed ':a;N;$!ba;s/\n/|/g' $(echo "$@")
+}
+
+
+#===========================================
+#       Variables
+#===========================================
+IMAGE_PREFIX=${IMAGE_PREFIX:-}
+TMP_FILE_NAME=running-image.tmp
+LIST_FILE_NAME=execlusion_list
+EXECLUSION_IMAGE_LIST="$(cat $LIST_FILE_NAME)"
+CURRENT_RUNNING_IMAGE=`docker ps --format="{{.Image}}"`
+
+
+#===========================================
+# 	Process
+#===========================================
 log "Start the image deletion process..."
 
 log "Print execlusion_list..."
-cat $LIST_FILE_NAME
+echo $EXECLUSION_IMAGE_LIST
+
+log "Current running images..."
+echo $CURRENT_RUNNING_IMAGE
+echo $CURRENT_RUNNING_IMAGE > $TMP_FILE_NAME
 
 log "Print target images..."
+
+DELETE_TARGET=`docker images --format="{{.Repository}}:{{.Tag}}" | grep -v -E $(listToLine $LIST_FILE_NAME) | grep -v -E $(listToLine $TMP_FILE_NAME)`
 echo "$DELETE_TARGET"
+
 
 
 # Delete images
 log "Delete"
+
+
+# Post Process
+
