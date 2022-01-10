8efea630ce232bb227bdc9ba09892e5ce3db7ef3
oneum20 <63915349+oneum20@users.noreply.github.com>
2022-01-10 21:52:39 +0900

    Update container-scanning.sh
    - 변수 및 주석 정리

--- scripts/container-scanning-by-trivy/container-scanning.sh
+++ scripts/container-scanning-by-trivy/container-scanning.sh
@@ -2,6 +2,7 @@
 
 SCRIPT=$( basename "$0" )
 TRIVY_IMAGE_TAG=${TRIVY_IMAGE_TAG:-"latest"}
+CONTAINER_NAME=${CONTAINER_NAME:-"trivy"}
 IMAGE=$1
 MSG=(
         "Error : Docker image name is required."
@@ -14,25 +15,25 @@ MSG=(
 printf "Container Scanning Target : ${IMAGE} \n\n"
 
 # Do scanning
-RES="$(docker run -it -v /var/run/docker.sock:/var/run/docker.sock -u root --privileged --name trivy aquasec/trivy:${TRIVY_IMAGE_TAG}  ${IMAGE}  )"
+RES="$(docker run -it -v /var/run/docker.sock:/var/run/docker.sock -u root --privileged --name ${CONTAINER_NAME} aquasec/trivy:${TRIVY_IMAGE_TAG}  ${IMAGE}  )"
 
 printf "\n>> Remove container : "
-docker rm -f trivy
+docker rm -f ${CONTAINER_NAME}
 
 # ERROR
-if [[ $( echo "$RES" | grep 'Error' | wc -l ) -ne 0 ]] ; then
-        echo "$RES"
+if [[ $( echo "${RES}" | grep 'Error' | wc -l ) -ne 0 ]] ; then
+        echo "${RES}"
         exit 1;
 fi
 
 
-# All mode
+# mode
 if [[ ! -z  $2  ]] && [[ $2 -eq '-a' ]] ; then
         echo ">> All mode"
 else
-        RES=$( echo "$RES" | grep -B 2 Total )
+        RES=$( echo "${RES}" | grep -B 2 Total )
 fi
 
 # Print result
-echo "$RES"
+echo "${RES}"
 
